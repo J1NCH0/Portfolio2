5cfb4a6f8d234756d7f31c1e2b2495f8c0a4d27c
Davit Jincharadze <davitjincharadze0128@gmail.com>
2023-03-31 15:20:53 +0400

    Responsive Half Done

--- script/script.js
+++ script/script.js
@@ -1,2 +1,15 @@
-font-size: 20px;
-line-height: 30px;
+'use strict'
+
+const element = document.querySelector('.secondary__button-regular');
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+function handleMediaQuery(event) {
+    if (event.matches) {
+        element.classList.add('secondary__button-small');
+    } else {
+        element.classList.remove('secondary__button-regular');
+    }
+}
+
+mediaQuery.addListener(handleMediaQuery);
+handleMediaQuery(mediaQuery);
